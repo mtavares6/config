6930a4002261e85b645ca5ffbce341644b558868
mtavares6 <miguel.tavares@carrotincentives.com>
2025-04-03 11:32:33 +0100

    added key bindings tmux and vim

--- nvim/lua/custom/keymaps/init.lua
+++ nvim/lua/custom/keymaps/init.lua
@@ -1,17 +1,32 @@
 -- Neogit
 local neogit = require 'neogit'
 
+--vim.api.nvim_set_keymap('i', '{', '{}<Left><CR><CR><Up><Tab>', { noremap = true, silent = true })
+
 vim.keymap.set('n', '<leader>go', neogit.open, { silent = true, noremap = true })
 
 vim.keymap.set('n', '<leader>gc', ':Neogit commit<CR>', { silent = true, noremap = true })
-
-vim.keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { silent = true, noremap = true })
 
 vim.keymap.set('n', '<leader>gP', ':Neogit push<CR>', { silent = true, noremap = true })
 
 vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<CR>', { silent = true, noremap = true })
 
-vim.keymap.set('n', '<leader>gB', ':G blame<CR>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { silent = true, noremap = true })
+
+-- TS
+vim.keymap.set('n', '<leader>io', function()
+  vim.lsp.buf.execute_command {
+    command = '_typescript.organizeImports',
+    arguments = { vim.api.nvim_buf_get_name(0) },
+  }
+end, { desc = 'Organize Imports (TS)' })
+
+vim.keymap.set('n', '<leader>ia', function()
+  vim.lsp.buf.code_action {
+    context = { only = { 'source.addMissingImports.ts' } },
+    apply = true,
+  }
+end, { desc = 'Import Missing Imports (TS)' })
 
 -- Java
 vim.keymap.set('n', '<leader>co', "<Cmd>lua require'jdtls'.organize_imports()<CR>", { desc = 'Organize Imports' })
@@ -20,7 +35,30 @@ vim.keymap.set('v', '<leader>crv', "<Esc><Cmd>lua require('jdtls').extract_varia
 vim.keymap.set('n', '<leader>crc', "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = 'Extract Constant' })
 vim.keymap.set('v', '<leader>crc', "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", { desc = 'Extract Constant' })
 vim.keymap.set('v', '<leader>crm', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", { desc = 'Extract Method' })
+-- Filetype-specific keymaps (these can be done in the ftplugin directory instead if you prefer)
+vim.keymap.set('n', '<leader>jo', function()
+  if vim.bo.filetype == 'java' then
+    require('jdtls').organize_imports()
+  end
+end)
 
+vim.keymap.set('n', '<leader>ju', function()
+  if vim.bo.filetype == 'java' then
+    require('jdtls').update_projects_config()
+  end
+end)
+
+vim.keymap.set('n', '<leader>jc', function()
+  if vim.bo.filetype == 'java' then
+    require('jdtls').test_class()
+  end
+end)
+
+vim.keymap.set('n', '<leader>jm', function()
+  if vim.bo.filetype == 'java' then
+    require('jdtls').test_nearest_method()
+  end
+end)
 -- Debugging
 vim.keymap.set('n', '<leader>bb', "<cmd>lua require'dap'.toggle_breakpoint()<cr>")
 vim.keymap.set('n', '<leader>bc', "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>")
@@ -31,6 +69,10 @@ vim.keymap.set('n', '<leader>dc', "<cmd>lua require'dap'.continue()<cr>")
 vim.keymap.set('n', '<leader>dj', "<cmd>lua require'dap'.step_over()<cr>")
 vim.keymap.set('n', '<leader>dk', "<cmd>lua require'dap'.step_into()<cr>")
 vim.keymap.set('n', '<leader>do', "<cmd>lua require'dap'.step_out()<cr>")
+vim.keymap.set('n', '<space>?', function()
+  require('dapui').eval(nil, { enter = true })
+end)
+
 vim.keymap.set('n', '<leader>dd', function()
   require('dap').disconnect()
   require('dapui').close()
